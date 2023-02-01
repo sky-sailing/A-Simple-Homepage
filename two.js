749935f5cd630b7ddb081f0fe9cf23b16692109f
Nightingode <24577548+Melencolia11514@users.noreply.github.com>
2023-02-02 00:09:02 +0800

    Add files via upload

---
+++ two.js
@@ -0,0 +1,19 @@
+function toggleContent() {
+    var title = document.querySelector(".menu");
+    var content = document.querySelector(".container");
+    var backBtn = document.querySelector(".back-btn");
+
+    title.style.display = "none";
+    content.style.display = "inline";
+    backBtn.style.display = "inline";
+  }
+
+  function back() {
+    var title = document.querySelector(".menu");
+    var content = document.querySelector(".container");
+    var backBtn = document.querySelector(".back-btn");
+
+    title.style.display = "flex";
+    content.style.display = "none";
+    backBtn.style.display = "none";
+  }
