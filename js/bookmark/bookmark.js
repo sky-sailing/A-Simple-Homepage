f5f298bfab7599ef38562dea615310808c9ed6fd
Laus Deo <24577548+Melencolia11514@users.noreply.github.com>
2023-02-17 17:47:08 +0800

    Add files via upload

--- js/bookmark/bookmark.js
+++ js/bookmark/bookmark.js
@@ -1,16 +1,19 @@
 // 获取页面的所有元素
-var page = document.body.cloneNode(true);
+var hupu = document.getElementsByClassName("hupu");
+var ithome = document.getElementsByClassName("ithome");
+var zhibo8 = document.getElementsByClassName("zhibo8");
 var pageCount = 1;
-var hupu = document.getElementById("hupu");
-var ithome = document.getElementById("ithome");
-var zhibo8 = document.getElementById("zhibo8");
+var page = document.body.cloneNode(true);
 
-// function topPage() {
-//     // 创建新的页面元素
-//     var newPage = document.createElement("div");
-//     newPage.innerHTML = page.innerHTML;
-//     document.body.insertBefore(newPage, document.body.firstChild);
-// }
+function change() {
+    if ((screen.width) <= 600) {
+        for (let i = 0; i < hupu.length; i++) {
+            hupu[i].href = "https://m.hupu.com/";
+            ithome[i].href = "https://m.ithome.com/";
+            zhibo8[i].href = "https://m.zhibo8.cc/";
+        };
+    };
+}
 
 function bottomPage() {
     // 创建新的页面元素
@@ -19,12 +22,15 @@ function bottomPage() {
     document.body.appendChild(newPage);
 }
 
-function change() {
-    if ((screen.width) <= 600) {
-        hupu.href = "https://m.hupu.com/";
-        ithome.href = "https://m.ithome.com/";
-        zhibo8.href = "https://m.zhibo8.cc/";
-    };
+// function topPage() {
+//     // 创建新的页面元素
+//     var newPage = document.createElement("div");
+//     newPage.innerHTML = page.innerHTML;
+//     document.body.insertBefore(newPage, document.body.firstChild);
+// }
+
+window.onload = function () {
+    change();
 }
 
 window.onscroll = function () {
@@ -39,9 +45,4 @@ window.onscroll = function () {
     // if (window.scrollY <= 0) {
     //     topPage();
     // };
-}
-
-
-window.onload = function () {
-    change();
 }
