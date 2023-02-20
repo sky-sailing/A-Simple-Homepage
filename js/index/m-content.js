f7923a17020b9d212bc7efda84f39f10e4a740f6
Laus Deo <24577548+Melencolia11514@users.noreply.github.com>
2023-02-20 12:25:54 +0800

    Add files via upload

--- js/index/m-content.js
+++ js/index/m-content.js
@@ -1,19 +1,28 @@
-if (window.innerWidth < 650) {
-  function toggleContent() {
-    var title = document.querySelector(".menu");
-    var content = document.querySelector(".content");
-    var backBtn = document.querySelector(".back-btn");
-    title.style.display = "none";
-    content.style.display = "inline";
-    backBtn.style.display = "inline";
-  }
+// if (window.innerWidth < 650) {
+//   function toggleContent() {
+//     var title = document.querySelector(".menu");
+//     // var content = document.querySelector(".content");
+//     // var backBtn = document.querySelector(".back-btn");
+//     // title.style.display = "none";
+//     // content.style.display = "inline";
+//     // backBtn.style.display = "inline";
+//   }
 
-  function back() {
-    var title = document.querySelector(".menu");
-    var content = document.querySelector(".content");
-    var backBtn = document.querySelector(".back-btn");
-    title.style.display = "flex";
-    content.style.display = "none";
-    backBtn.style.display = "none";
-  }
+// //   function back() {
+// //     var title = document.querySelector(".menu");
+// //     var content = document.querySelector(".content");
+// //     // var backBtn = document.querySelector(".back-btn");
+// //     // title.style.display = "flex";
+// //     // content.style.display = "none";
+// //     // backBtn.style.display = "none";
+// //   }
+// }
+
+window.onload = function () {
+  if (window.innerWidth < 650) {
+    var titles = document.getElementsByClassName("titles");
+    for (let i = 0; i < titles.length; i++) {
+      titles[i].target = "_blank";
+    };
+  };
 }
