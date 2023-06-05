4be4ac2a5d92b0f7ac8f3b18aa88e4e3ae5b0ea2
Laus Deo <24577548+l-deo@users.noreply.github.com>
2023-06-05 10:05:14 +0800

    Add files via upload

---
+++ js/bookmark/m-bookmark.js
@@ -0,0 +1,15 @@
+var hupu = document.getElementById("hupu");
+var ithome = document.getElementById("ithome");
+var zhibo8 = document.getElementById("zhibo8");
+
+function change() {
+    if ((screen.width) <= 600) {
+        hupu.href = "https://m.hupu.com/";
+        ithome.href = "https://m.ithome.com/";
+        zhibo8.href = "https://m.zhibo8.cc/";
+    };
+}
+
+window.onload = function () {
+    change();
+}
