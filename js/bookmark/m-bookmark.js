0f245815369774c51db9e06bc45bb45147d08fbb
Laus Deo <24577548+l-deo@users.noreply.github.com>
2024-12-14 17:54:52 +0800

    Update m-bookmark.js

--- js/bookmark/m-bookmark.js
+++ js/bookmark/m-bookmark.js
@@ -6,7 +6,7 @@ function change() {
     if ((screen.width) <= 600) {
         hupu.href = "https://m.hupu.com/";
         ithome.href = "https://m.ithome.com/";
-        zhibo8.href = "https://m.zhibo8.cc/";
+        zhibo8.href = "https://m.zhibo8.com/";
     };
 }
 
